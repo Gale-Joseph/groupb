54c302427e50ec18957c7b4857d7371d44b2840b
Joseph Gale <gal16026@byui.edu>
2020-01-30 12:26:04 -0600

    Updating comments on DeskQuote.cs class

--- MegaDesk-Gale/DeskQuote.cs
+++ MegaDesk-Gale/DeskQuote.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* This class is used to calculate the price of a desk;
+ * A DeskQuote object and Desk object are created in AddQuote.cs once the user starts entering data;
+ * As the user enters data, that data is added to the DeskQuote object and Desk object;
+ * Most of these functions are executed when the final quote is displayed in DisplayQuote.cs
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,29 +15,30 @@ namespace MegaDesk_Gale
    
     public class DeskQuote
     {
-
+        /*member variables: */
         string quoteDate = DateTime.Now.ToString();
-        //general members:
-        public Desk desk { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string currentDate;
-        
-        
-        //desk specs
+
+        /* standard delivery is 14 days and accrues no extra costs;
+         * however, if deliveryTime is different, the cost will be
+         * adjusted in the function getDeliveryPrice() listed below;*/
         public int deliveryTime = 14;
 
-        //unit price list
+        /*pricing structure variables:*/
         public int basePrice = 200;
-        int saPrice = 1;
+        int saPrice = 1; //surface area price: $1 per square inch
         public int drawerPrice = 50;
-        public int surfacePrice;
-        public int deliveryPrice { get; set; }        
-        
-        //methods
+        public int surfacePrice; //the price of the material for desk surface
+        public int deliveryPrice { get; set; }
 
+        /*constructor*/
+        public Desk desk { get; set; }
 
-     
+        //methods  
+        /* getDeliveryPrice is not particularly written well and gets cumbersome to fill out, but
+         * as of 1/30/20 it makes the program function correctly*/
         public int getDeliveryPrice(int width, int depth, int deliveryTime)
         {
             int sa = width * depth;
@@ -96,13 +103,14 @@ namespace MegaDesk_Gale
             int surfacePrice = (int)desk.Surface;
             return surfacePrice;
         }
+        /* getTotal will be rewritten to get rid of "magic" numbers in price calculation;
+         * this will be the function that also stores the quote to quotes.json */
         public int getTotal(Desk desk)
         {
             saPrice *= desk.Width * desk.Depth;
             drawerPrice *= desk.Drawers;
             surfacePrice = getSurfacePrice(desk);
-            /*deliveryPrice = getDeliveryPrice(desk.Width,desk.Depth,this.deliveryTime);*/
-            deliveryPrice = 100;
+            deliveryPrice = 100;//wrong number, needs to be redone: getDeliveryPrice(desk.width,desk.depth,this.deliveryTime)
             int total = basePrice + saPrice + drawerPrice + surfacePrice + deliveryPrice;            
 
             return total;
