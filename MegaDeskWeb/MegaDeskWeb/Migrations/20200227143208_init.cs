6d891d625fc3e5548e103b109be24761d7f7387b
Megan Hendrickson <dearmegs@gmail.com>
2020-02-27 12:39:52 -0600

    Validation and Styles

---
+++ MegaDeskWeb/MegaDeskWeb/Migrations/20200227143208_init.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDeskWeb.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "width",
+                table: "Quote",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "surfaceMaterial",
+                table: "Quote",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastname",
+                table: "Quote",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstname",
+                table: "Quote",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "depth",
+                table: "Quote",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "width",
+                table: "Quote",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "surfaceMaterial",
+                table: "Quote",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastname",
+                table: "Quote",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstname",
+                table: "Quote",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "depth",
+                table: "Quote",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
--- MegaDeskWeb/MegaDeskWeb/Models/Quote.cs
+++ MegaDeskWeb/MegaDeskWeb/Models/Quote.cs
@@ -9,15 +9,44 @@ namespace MegaDeskWeb.Models
     public class Quote
     {
         public int ID { get; set; }
+        
+        [Display(Name = "Quote Date")]
         [DataType(DataType.Date)]
         public DateTime quoteDate { get; set; }
+
+        [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string firstname { get; set; }
+
+        [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string lastname { get; set; }
-        public int width { get; set; }
-        public int depth { get; set; }
+
+        [Display(Name = "Width")]
+        [Range(1, 10000)]
+        public double width { get; set; }
+
+        [Display(Name = "Depth")]
+        [Range(1, 10000)]
+        public double depth { get; set; }
+        
+        [Display(Name = "Drawers")]
+        [Required]
         public int drawers { get; set; }
+
+        [Display(Name = "Surface Material")]
+        [Required]
         public string surfaceMaterial { get; set; }
+
+        [Display(Name = "Rush Days")]
+        [Required]
         public int rushDelivery { get; set; }
+
+        [Display(Name = "Quote Total")]
         public double total { get; set; }
     }
 }
